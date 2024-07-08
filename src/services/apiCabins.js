db78cf6da32344e23ed78b8609b66a9b25c9e7bc
codetanvir <info.tanvirx@gmail.com>
2024-07-08 21:39:58 +0600

    fecth cabin data thrrough react-query setup vid 4/27

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -2,11 +2,7 @@ import supabase from "./supabase";
 
 export async function getCabins(){
 let { data, error } = await supabase.from('cabins').select('*')
-
-
 if(error){
-    console.log(error)
-    console.error(error);
     throw new Error('Cabins could not be loaded')
 }
 return data
