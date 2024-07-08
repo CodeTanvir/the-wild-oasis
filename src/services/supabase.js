75d5433dbdb53bfdff93bcaf0b66128b3c06d2c7
codetanvir <info.tanvirx@gmail.com>
2024-07-08 20:29:49 +0600

    react query setup vid 3/27

---
+++ src/services/supabase.js
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://iythqiiduytaljdvygwk.supabase.co';
+
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.\
+eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5dGhxaWlkdXl0YWxqZHZ5Z3drI\
+iwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAxNzU2ODQsImV4cCI6MjAzNTc1MTY4NH0\
+.yfzW62T5aVASiARz8XD9wl23NYdfrz4JZdULjaArNWs"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
