73fd3d8764679ba7cc751816f0b48a7820801496
CodeTanvir <info.tanvirx@gmail.com>
2024-10-02 15:53:33 +0800

    Modal setup

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import Button from "../../ui/Button"
+import CreateCabinForm from "./CreateCabinForm"
+
+function AddCabin() {
+    const [isOpenModal, setIsOpenModal] = useState(false)
+    return (
+        <div>
+         <Button onClick={()=> setIsOpenModal((show)=> !show)}
+      >Add new cabin</Button>
+      {isOpenModal && <CreateCabinForm />}
+        </div>
+    )
+}
+
+export default AddCabin
